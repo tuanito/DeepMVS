703f08cebe36aaf5a5941c390779e5a879a1dca8
Tuan <tuan.nguyen@invisart.fr>
2020-12-05 02:11:23 +0100

    WIP.

--- python/download_trained_model.py
+++ python/download_trained_model.py
@@ -7,13 +7,13 @@ def download_trained_model(path = None):
 		path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "model")
 	if not os.path.isdir(path):
 		os.mkdir(path)
-	print "Downloading trained model..."
+	print ('Downloading trained model...')
 	subprocess.call(
 		"cd {:} ;".format(path) + 
 		"wget -O DeepMVS_final.model https://www.dropbox.com/s/dcaip7n3z0yk7lx/DeepMVS_final.model?dl=1 ;",
 		shell = True
 	)
-	print "Successfully downloaded trained model."
+	print ("Successfully downloaded trained model.")
 
 if __name__ == "__main__":
 	download_trained_model()
--- python/download_training_datasets.py
+++ python/download_training_datasets.py
@@ -10,16 +10,16 @@ import h5py
 from lz4.block import decompress
 
 def download_demon_datasets(path):
-	print """
-===================================
-Note: DeMoN dataset is not created by us. If you use these datasets in your work, please visit the url below for information about citations. 
-https://github.com/lmb-freiburg/demon/blob/master/datasets/download_traindata.sh
-===================================
-	"""
+#	print """
+#===================================
+#Note: DeMoN dataset is not created by us. If you use these datasets in your work, please visit the url below for information about citations. 
+#https://github.com/lmb-freiburg/demon/blob/master/datasets/download_traindata.sh
+#===================================
+#	"""
 	DATASET_NAMES = ["sun3d", "rgbd", "mvs", "scenes11"]
 	for dataset_name in DATASET_NAMES:
 		if not os.path.exists(os.path.join(path, "{:}_train.tgz".format(dataset_name))):
-			print "Downloading {:} dataset...".format(dataset_name)
+			print ('Downloading {:} dataset...'.format(dataset_name))
 			subprocess.call(
 					"cd {:} ;".format(path) +
 					"wget https://lmb.informatik.uni-freiburg.de/data/demon/traindata/{:}_train.tgz ;".format(dataset_name) +
@@ -27,22 +27,30 @@ https://github.com/lmb-freiburg/demon/blob/master/datasets/download_traindata.sh
 					shell = True
 				)
 
-	print "Converting DeMoN dataset into the format required by DeepMVS..."
+	print ("Converting DeMoN dataset into the format required by DeepMVS...")
+        # These datasets have been skipped : scenes11_train, sun3d_train_0.4m_to_0.8m, sun3d_train_1.6m_to_infm
 	SUB_DATASET_NAMES = ([
 		"mvs_achteck_turm", "mvs_breisach", "mvs_citywall", 
 		"rgbd_10_to_20_3d_train", "rgbd_10_to_20_handheld_train", "rgbd_10_to_20_simple_train", "rgbd_20_to_inf_3d_train", "rgbd_20_to_inf_handheld_train", "rgbd_20_to_inf_simple_train",
-		"scenes11_train", 
-		"sun3d_train_0.01m_to_0.1m", "sun3d_train_0.1m_to_0.2m", "sun3d_train_0.2m_to_0.4m", "sun3d_train_0.4m_to_0.8m", "sun3d_train_0.8m_to_1.6m", "sun3d_train_1.6m_to_infm"
+		##"scenes11_train", 
+		"sun3d_train_0.01m_to_0.1m", 
+                ##"sun3d_train_0.1m_to_0.2m", 
+		"sun3d_train_0.2m_to_0.4m", 
+	        ##"sun3d_train_0.4m_to_0.8m", 
+		"sun3d_train_0.8m_to_1.6m"#, 
+	        ##"sun3d_train_1.6m_to_infm"
 	])
 	for dataset_name in SUB_DATASET_NAMES:
-		print "Converting {:}.h5 ...".format(dataset_name)
+		#print "Converting {:}.h5 ...".format(dataset_name)
+		print ('Converting {:}.h5 ...'.format(dataset_name))
 		if not os.path.isdir(os.path.join(path, dataset_name)):
 			os.mkdir(os.path.join(path, dataset_name))
 		file = h5py.File(os.path.join(path, "{:}.h5".format(dataset_name)), "r")
 		
 		num_images = []
 		for (seq_idx, seq_name) in enumerate(file):
-			print "Processing sequence {:d}/{:d}".format(seq_idx, len(file))
+			#print "Processing sequence {:d}/{:d}".format(seq_idx, len(file))
+			print ('Processing sequence {:d}/{:d}'.format(seq_idx, len(file)))
 			if not os.path.isdir(os.path.join(path, dataset_name, "{:04d}".format(seq_idx))):
 				os.mkdir(os.path.join(path, dataset_name, "{:04d}".format(seq_idx)))
 			if not os.path.isdir(os.path.join(path, dataset_name, "{:04d}".format(seq_idx), "images")):
@@ -84,7 +92,8 @@ https://github.com/lmb-freiburg/demon/blob/master/datasets/download_traindata.sh
 
 def download_GTAV_datasets(path):
 	if not os.path.exists(os.path.join(path, "GTAV_720.tar.gz")):
-		print "Downloading GTAV_720 dataset..."
+		#print "Downloading GTAV_720 dataset..."
+		print ('Downloading GTAV_720 dataset...')
 		subprocess.call(
 				"cd {:} ;".format(path) +
 				"wget -O GTAV_720.tar.gz https://filebox.ece.vt.edu/~jbhuang/project/deepmvs/mvs-syn/GTAV_720.tar.gz ;" +
@@ -92,7 +101,7 @@ def download_GTAV_datasets(path):
 				shell = True
 			)
 	if not os.path.exists(os.path.join(path, "GTAV_540.tar.gz")):
-		print "Downloading GTAV_540 dataset..."
+		print ('Downloading GTAV_540 dataset...')
 		subprocess.call(
 				"cd {:} ;".format(path) + 
 				"wget -O GTAV_540.tar.gz https://filebox.ece.vt.edu/~jbhuang/project/deepmvs/mvs-syn/GTAV_540.tar.gz ;" +
@@ -107,12 +116,12 @@ def download_training_datasets(path = None):
 			os.mkdir(os.path.join(ROOT_DIR, "dataset"))
 		if not os.path.isdir(os.path.join(ROOT_DIR, "dataset", "train")):
 			os.mkdir(os.path.join(ROOT_DIR, "dataset", "train"))
-		download_demon_datasets(os.path.join(ROOT_DIR, "dataset", "train"))
+		#download_demon_datasets(os.path.join(ROOT_DIR, "dataset", "train"))
 		download_GTAV_datasets(os.path.join(ROOT_DIR, "dataset", "train"))
 	else:
 		download_demon_datasets(path)
 		download_GTAV_datasets(path)
-	print "Finished downloading training datasets."
+	print ('Finished downloading training datasets.')
 
 if __name__ == "__main__":
 	download_training_datasets()
